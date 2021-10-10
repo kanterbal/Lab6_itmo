f63e087baa3dfa223520ec039033a9225cdde48c
Sergei Kanter <sdkanter@outlook.com>
2021-10-10 11:49:08 +0200

    Ex2 done.

---
+++ Ex2/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ex2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string s = Console.ReadLine();
+            s = s.ToLower();
+            bool p = true;
+            string pal = "";
+            string[] stringArray = s.Split();
+            foreach (string str in stringArray)
+            {
+                pal += str;
+            }
+            //Console.WriteLine(pal);
+            for (int i = 0; i < pal.Length / 2; i++)
+            {
+                if (pal[i] != pal[pal.Length - 1 - i])
+                {
+                    p = false;
+                }
+            }
+            Console.WriteLine(p);
+            Console.ReadKey();
+        }
+    }
+}
