b4ed4811cf2a5ccaed4c68b98f8e9aec8b01ab8f
BIM <BIM@DESKTOP-B03BBT4>
2021-10-10 11:22:29 +0200

    Добавьте файлы проекта.

---
+++ Ex1/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ex1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string startString = Console.ReadLine();
+            string res = "";
+            int max = 0;
+            string[] stringArray = startString.Split();
+            foreach (string s in stringArray)
+            {
+                if (s.Length >= max)
+                {
+                    max = s.Length;
+                    res = s;
+                }
+            }
+            Console.WriteLine("Саммое длинное слово - {0}, его длина = {1}", res, max);
+            Console.ReadKey();
+        }
+    }
+}
